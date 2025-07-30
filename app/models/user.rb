8c24ccc576021f07a5e31cd684c0490d6011f5c8
CyberIce2077 <146313291+CyberIce2077@users.noreply.github.com>
2025-07-31 00:54:49 +0300

    Task 10 login with google (#11)
    * log in with google
    
    * log in with google css

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,36 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable and :trackable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+         :confirmable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :recipients, dependent: :destroy, foreign_key: :creator_id
 
   enum :role, { user: 0, admin: 1 }, suffix: true
 
+  validate :email_not_taken_by_google_user
+
+  def self.from_omniauth(auth)
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.confirmed_at = Time.zone.now
+    end
+  end
+
   # def send_devise_notification(notification, *args)
   #   sleep 1 # Simulate a delay for the email to be sent
   #   devise_mailer.send(notification, self, *args).deliver_later(queue: :mailers, attempts: 5)
   # end
+
+  private
+
+  def email_not_taken_by_google_user
+    user = User.find_by(email:)
+
+    if user&.provider == 'google_oauth2'
+      errors.add(:base, "The account is linked to a Google account. Please sign in with Google.")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions'
+  }
 
   authenticate :user, ->(user) { user.confirmed? && user.admin_role? } do
     mount Sidekiq::Web => '/sidekiq'
