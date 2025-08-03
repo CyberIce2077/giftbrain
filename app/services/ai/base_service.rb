6682b1394aee8dccd12b0b080679be45269fa1b7
CyberIce2077 <gnaydev@gmail.com>
2025-08-03 21:22:14 +0300

    added unpermitted model error

--- app/services/ai/base_service.rb
+++ app/services/ai/base_service.rb
@@ -2,6 +2,7 @@ module Ai
   class BaseService < BaseService
     class UnsafePromptError < StandardError; end
     class MissingModelError < StandardError; end
+    class NotPermittedModelError < StandardError; end
 
     WEB_SITES = "Amazon, Etsy or Aliexpress"
 
