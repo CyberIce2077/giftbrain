33600485ca641b0954c6a794cce1e0f776b7d2c0
CyberIce2077 <gnaydev@gmail.com>
2025-08-04 15:57:12 +0300

    turbo confirm

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "sortablejs", to: "https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/modular/sortable.core.esm.js"
 pin "fullcalendar", to: "https://cdn.skypack.dev/fullcalendar@6.1.8"
+pin "@rolemodel/turbo-confirm", to: "@rolemodel--turbo-confirm.js" # @2.1.1
