0342b13b8f150493194559ce491b0556f693bdb7
CyberIce2077 <gnaydev@gmail.com>
2025-08-20 16:34:53 +0300

    add missing validation

--- app/models/recipient_idea.rb
+++ app/models/recipient_idea.rb
@@ -2,5 +2,6 @@ class RecipientIdea < ApplicationRecord
   belongs_to :recipient
   belongs_to :idea
 
+  validates :priority, presence: true
   validates :recipient_id, uniqueness: { scope: :idea_id }
 end
