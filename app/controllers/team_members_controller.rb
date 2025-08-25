f28352fdd2d2e8da9f0bcab5374f4047cee6d826
CyberIce2077 <gnaydev@gmail.com>
2025-08-25 14:45:46 +0300

    temp

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -3,15 +3,13 @@ class TeamMembersController < ApplicationController
     team_member = find_team_member
     authorize(team_member)
 
-    team = team_member.team
-
     if team_member.destroy
       flash[:notice] = "Deleted successfully"
     else
       flash[:warning] = team_member.errors.full_messages.to_sentence
     end
 
-    render "team_members/destroy", locals: { team:, team_member: }
+    render "team_members/destroy", locals: { team_member: }
   end
 
   private
