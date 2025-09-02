18d2335161b79b75817b43b781adf29e63d106ec
CyberIce2077 <gnaydev@gmail.com>
2025-09-02 21:55:35 +0300

    migrate to simple_calendar ruby from fullcalendar js
    temp
    
    local calendar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,26 +8,12 @@ class HomeController < ApplicationController
   def calendar
     authorize :home
 
-    respond_to do |format|
-      format.html { render "home/calendar" }
-      format.turbo_stream do
-        recipients = load_recipients
-        render "home/calendar", locals: { recipients:, teams: load_teams(recipients) }, layout: false
-      end
-      format.json do
-        render json: load_recipients, each_serializer: RecipientSerializer
-      end
-    end
-  end
+    date = params.fetch(:date, Time.zone.now).to_date
 
-  private
+    recipients =
+      policy_scope(Recipient).where(event_date: date.beginning_of_month..date.end_of_month)
+    teams = policy_scope(Team).where(recipient: recipients)
 
-  def load_recipients
-    policy_scope(Recipient).where(event_date: params[:start]..params[:end])
-                           .order(id: :desc)
-  end
-
-  def load_teams(recipients)
-    policy_scope(Team).where(recipient: recipients)
+    render "home/calendar", locals: { date:, recipients:, teams: }
   end
 end
