df991a7eb0e9a68edf08d55b505b3a02d1c7407e
CyberIce2077 <gnaydev@gmail.com>
2025-09-14 16:31:19 +0300

    hide spinner when back on previous page

--- app/javascript/controllers/spinner_controller.js
+++ app/javascript/controllers/spinner_controller.js
@@ -2,6 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="spinner"
 export default class extends Controller {
+  connect() {
+    document.getElementById("spinner").style.display = "none"
+  }
+
   disconnect() {
     document.getElementById("spinner").style.display = "none"
   }
